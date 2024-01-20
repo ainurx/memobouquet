2be6b0395c60962cac1f079563fe63032b32a80f
ainurx <ainurrahmat1@gmail.com>
2024-01-20 19:06:40 +0800

    [master] assets

--- src/app/components/MainNavbar.jsx
+++ src/app/components/MainNavbar.jsx
@@ -1,5 +1,6 @@
 'use client'
 import React from 'react'
+import Image from 'next/image';
 
 // External dep
 import Container from 'react-bootstrap/Container';
@@ -8,9 +9,11 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function MainNavbar(){
     return(
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className="m-bg-primary">
             <Container>
-                <Navbar.Brand href="#home">Memo.bouquet</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <Image src="/logo-transparent.png" alt="memo.bouquet" width={64} height={64} />
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="py-3 text-center">memo.bouquet</h1>
+      <Image className="mx-auto d-block" src='/memobouquet-logo.png' alt='memo.bouquet' width={320} height={320} />
+      <h2 className="py-3 text-center">for your every special moment 💖</h2>
     </main>
   )
 }
